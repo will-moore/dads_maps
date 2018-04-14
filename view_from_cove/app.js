e1e8dd1cb8cd12cb32e239fd54db873deb4a3225
William Moore <w.moore@dundee.ac.uk>
2018-04-14 23:27:44 +0100

    Add hill labels as links to show hills

--- view_from_cove/app.js
+++ view_from_cove/app.js
@@ -2,6 +2,77 @@
 
 var osMap;
 
+var COVE_LONG = 180964;
+var COVE_LAT = 891029;
+
+var left_island_offset = -846;   // pixels
+var left_island_angle = 105;    // degrees
+var triangulation_offset = -2618;
+var triangulation_angle = 140.9;
+
+var hills = [
+    {name: "Sail Mhor", height: 767, lon: 203299.66490821, lat: 888706.16754589},
+    {name: "An Teallach", height: 1060, lon: 206407.30236777, lat: 883686.36611983},
+    {name: "Beinn Dearg Mor", height: 906, lon: 203211.86353608, lat: 879935.19828485},
+    {name: "A' Mhaighdean", height: 967, lon: 200776.84972172, lat: 874892.68293556},
+    {name: "Slioch", height: 980, lon: 200496.86796237, lat: 868874.02056833},
+    {name: "Meall Mheinnidh", height: 722, lon: 195484.83281297, lat: 874848.31605644},
+    {name: "Beinn Airigh Charr", height: 792, lon: 193029.83281297, lat: 876178.31605644},
+    {name: "Meall a' Ghiuthais", height: 887, lon: 197609.83281297, lat: 863428.31605644},
+    {name: "Ruadh-stac Mor", height: 1010, lon: 195141.56998945, lat: 861154.90166647},
+    {name: "Sail Mhor (Beinn Eighe)", height: 980, lon: 193816.56998945, lat: 860584.90166647},
+]
+
+
+var pointToDegrees = function(map_point) {
+    var dx = map_point.lon - COVE_LONG;
+    var dy = map_point.lat - COVE_LAT;
+    
+    var distance = Math.sqrt((dx * dx) + (dy * dy));
+    console.log('distance', distance);
+    return Math.atan2(dx, dy) * 180 / Math.PI;
+};
+
+
+var degreesToPoint = function(degrees) {
+    // pick some distance, then calculate x and y
+    var drid_distance = 60000;
+    var radians = degrees * Math.PI / 180;
+    var dx = Math.sin(radians) * drid_distance;
+    var dy = Math.cos(radians) * drid_distance;
+    console.log('dx, dy', dx, dy);
+
+    var map_point = {lon: COVE_LONG + dx,
+                     lat: COVE_LAT + dy};
+    return map_point;
+}
+
+
+var offsetToDegrees = function(offset) {
+    // Pixels offset on horizon to compass bearing from Cove
+    var offset_range = triangulation_offset - left_island_offset;
+    var offset_fraction = (offset - left_island_offset) / offset_range;
+    var angle = (offset_fraction * (triangulation_angle - left_island_angle)) + left_island_angle;
+    return angle;
+}
+
+var degreesToOffset = function(degrees) {
+    // Compass bearing from Cove to pixels offset for horizon
+    var angle_fraction = (degrees - left_island_angle) / (triangulation_angle - left_island_angle);
+    var offset_range = triangulation_offset - left_island_offset;
+    var offset = (angle_fraction * offset_range) + left_island_offset;
+    return offset;
+}
+
+var offsetHorizon = function(degrees) {
+
+    var offset = degreesToOffset(degrees);
+    console.log("OFFSET", offset);
+    document.getElementById("image_canvas").style.left = offset + 'px';
+    document.getElementById("scroll_container").style.left = '0px';
+}
+
+
 // This function creates the map and is called by the div in the HTML
 function init() {
 
@@ -14,14 +85,6 @@ function init() {
         fillColor: "#ff00ff",
         fillOpacity: 0
     };
-
-    var COVE_LONG = 180964;
-    var COVE_LAT = 891029;
-
-    var left_island_offset = -846;   // pixels
-    var left_island_angle = 105;    // degrees
-    var triangulation_offset = -2618;
-    var triangulation_angle = 140.9;
 
     var options;
     var vectorLayer;
@@ -36,7 +99,6 @@ function init() {
     // Create a new vector layer
     vectorLayer = new OpenLayers.Layer.Vector("Vector Layer");
     osMap.addLayer(vectorLayer);
-
     
 
     var drawLineFromCoveToPoint = function(map_point) {
@@ -50,54 +112,6 @@ function init() {
         vectorLayer.addFeatures([polygonFeature]);
         return polygonFeature;
     };
-
-
-    var pointToDegrees = function(map_point) {
-        var dx = map_point.lon - COVE_LONG;
-        var dy = map_point.lat - COVE_LAT;
-        
-        var distance = Math.sqrt((dx * dx) + (dy * dy));
-        console.log('distance', distance);
-        return Math.atan2(dx, dy) * 180 / Math.PI;
-    };
-
-
-    var degreesToPoint = function(degrees) {
-        // pick some distance, then calculate x and y
-        var drid_distance = 60000;
-        var radians = degrees * Math.PI / 180;
-        var dx = Math.sin(radians) * drid_distance;
-        var dy = Math.cos(radians) * drid_distance;
-        console.log('dx, dy', dx, dy);
-
-        var map_point = {lon: COVE_LONG + dx,
-                         lat: COVE_LAT + dy};
-        return map_point;
-    }
-
-
-    var offsetToDegrees = function(offset) {
-        // Pixels offset on horizon to compass bearing from Cove
-        var offset_range = triangulation_offset - left_island_offset;
-        var offset_fraction = (offset - left_island_offset) / offset_range;
-        var angle = (offset_fraction * (triangulation_angle - left_island_angle)) + left_island_angle;
-        return angle;
-    }
-    
-    var degreesToOffset = function(degrees) {
-        // Compass bearing from Cove to pixels offset for horizon
-        var angle_fraction = (degrees - left_island_angle) / (triangulation_angle - left_island_angle);
-        var offset_range = triangulation_offset - left_island_offset;
-        var offset = (angle_fraction * offset_range) + left_island_offset;
-        return offset;
-    }
-
-    var offsetHorizon = function(degrees) {
-
-        var offset = degreesToOffset(degrees);
-        document.getElementById("image_canvas").style.left = offset + 'px';
-        document.getElementById("scroll_container").style.left = '0px';
-    }
 
 
     var drag_start_x;
@@ -120,6 +134,15 @@ function init() {
 
     document.getElementById("image_canvas").addEventListener("click", function( event ) {
         console.log(event.offsetX, event.target);
+        if (event.target.tagName.toLowerCase() === 'a') {
+            // get the data-index attribute
+            var hill_index = event.target.dataset.index;
+            var hill = hills[hill_index];
+            drawLineFromCoveToPoint(hill);
+            offsetHorizon(pointToDegrees(hill));
+            osMap.setCenter(new OpenSpace.MapPoint(hill.lon, hill.lat));
+            return false;
+        }
         // var click_left = event.offsetX 
         var degrees = offsetToDegrees(-event.offsetX);
         console.log('degrees', degrees);
@@ -141,12 +164,23 @@ function init() {
         onClick: function (event) {
             // The mouse position is converted into a map position via the Map.getLonLatFromViewportPx(). This returns a point in the coordinate system of the map base layer, in our case this is British National Grid.
             var pt = osMap.getLonLatFromViewPortPx(event.xy);
+            console.log('click', pt);
             drawLineFromCoveToPoint(pt);
-            offsetHorizon( pointToDegrees(pt) );
+            var degrees = pointToDegrees(pt);
+            console.log('DEGREES', degrees);
+            offsetHorizon(degrees);
         },
     });
 
     // Add the clickcontrol to the map
     osMap.addControl(clickcontrol);
 
+    // Add labels to horizon image
+    var html = hills.map(function(hill, index){
+        var offset = -degreesToOffset(pointToDegrees(hill));
+        return "<a data-index='" + index + "' href='#' class='hill_label' style='left:" + offset + "px'>" + hill.name + "</a>";
+    }).join("");
+    console.log(html);
+    document.getElementById("image_canvas").insertAdjacentHTML("beforeend", html);
 }
+
